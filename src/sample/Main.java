59456bd247f1087d72addbea72a7952e38fba38b
yfp196 <onlypudge@live.com>
2020-12-05 02:52:38 -0600

    "TEST FILES, MAIN.java Functions and DS added"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,24 +20,52 @@ public class Main extends Application {
     //  Main Class variables  //
     public String sampleDataCSVName = "sampleDatasetA.csv";
     public String sampleDataTXTName = "sampleDatasetA.txt";
+    public String sampleEmployeesCSVName = "sampleEmployeesA.csv";
+    public String sampleEmployeesTXTName = "sampleEmployeesA.txt";
+    public String sampleOrdersCSVName = "sampleOrders.csv";
+    public String sampleOrdersTXTName = "sampleOrders.txt";
     public class GroceryItem {
         private String type;
         private String name;
         private double price;
         private int stock;
+        private int expDATE;
         @Override
         public String toString() {
-            return type + "," + name + "," + price + "," + stock + "\n"; }
+            return type + "," + name + "," + price + "," + stock + "," + expDATE + "\n"; }
+    }
+    public class User {
+        private String type;
+        private String name;
+        private String username;
+        private String password;
+        private String shifts;
+        @Override
+        public String toString() {
+            return type + "," + name + "," + username + "," + password + "," + shifts + "\n"; }
+    }
+    public class Order {
+        private String name;
+        private String list;
+        private double total;
+        private String role;
+        @Override
+        public String toString() {
+            return name + "," + list + "," + total + "," + role + "\n"; }
     }
     Map<String, GroceryItem> inventoryMap = new HashMap<String, GroceryItem>();
+    Map<String, User> userMap = new HashMap<String, User>();
+    Map<String, Order> orderMap = new HashMap<String, Order>();
     //      main Function        //
     public static void main(String[] args) { launch(args); }
     //      start Function      //
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Load Files
-        readInputFile(sampleDataTXTName);
-        writeInputFile(sampleDataCSVName, inventoryMap);
+        readEmployeesFile(sampleEmployeesTXTName);
+        readInventoryFile(sampleDataTXTName);
+        readOrdersFile(sampleOrdersTXTName);
+        writeInventoryFile(sampleDataCSVName, inventoryMap);
         printInventoryMap(inventoryMap);
         // Views
         Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
@@ -58,7 +86,35 @@ public class Main extends Application {
         primaryStage.show();
     }
     //      readInputFile Function      //
-    public void readInputFile(String csvFileName) throws FileNotFoundException {
+    public void readEmployeesFile(String csvFileName) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(csvFileName));
+        while(input.hasNextLine()) {
+            String line = input.nextLine();
+            String[] token = line.split(",");
+            User tempUser = new User();
+            tempUser.type = token[0];
+            tempUser.name = token[1];
+            tempUser.username = token[2];
+            tempUser.password = token[3];
+            tempUser.shifts = token[4];
+        }
+        input.close();
+    }
+    public void readOrdersFile(String csvFileName) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(csvFileName));
+        while(input.hasNextLine()) {
+            String line = input.nextLine();
+            String[] token = line.split(",");
+            Order tempOrder = new Order();
+            tempOrder.list = token[0];
+            tempOrder.name = token[1];
+            double tempPrice = Double.parseDouble(token[2]);
+            tempOrder.total = tempPrice;
+            tempOrder.role = token[3];
+        }
+        input.close();
+    }
+    public void readInventoryFile(String csvFileName) throws FileNotFoundException {
         Scanner input = new Scanner(new File(csvFileName));
         while(input.hasNextLine()) {
             String line = input.nextLine();
@@ -75,7 +131,7 @@ public class Main extends Application {
         input.close();
     }
     //      writeInputFile Function     //
-    public void writeInputFile(String csvFileName, Map hashMap) throws IOException {
+    public void writeInventoryFile(String csvFileName, Map hashMap) throws IOException {
         FileWriter writer = new FileWriter(sampleDataCSVName);
         hashMap.forEach((k,v) -> {
             try {
